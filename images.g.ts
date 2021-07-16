c86cbbc54d9f808c4ca05ab5f5ef98cd8091b5ec
ILIA4556 <87526128+ILIA4556@users.noreply.github.com>
2021-07-16 19:33:30 +0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "v@=gm1/?Lt~y%8(;8@z]":
+            case "12":return img`
+. . . . . f f f . f f f . . . . 
+. . . . . f 3 f . f 3 f . . . . 
+. . . . . f 3 f . f 3 f . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . f f f f f f f f f . . . 
+. . . . f f f f f f f f f . . . 
+. . . . f f 1 f f f 1 f f . . . 
+. . . . f f f f f f f f f . . . 
+. . . . f f f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,48 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    info.changeScoreBy(1)
+    music.baDing.play()
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorLockedNorth, function (sprite, location) {
+    info.setLife(3)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    info.changeLifeBy(-1)
+    music.powerDown.play()
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    tiles.setTilemap(tilemap`4321`)
+    tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleBlueCrystal)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairWest, function (sprite, location) {
+    game.over(true)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    effects.clearParticles(mySprite)
+})
+info.onLifeZero(function () {
+    game.over(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    mySprite.startEffect(effects.fire)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
+    tiles.setTilemap(tilemap`level3`)
+    tiles.placeOnRandomTile(mySprite, assets.tile`transparency16`)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+let mySprite: Sprite = null
+info.setLife(4)
+tiles.setTilemap(tilemap`4321`)
+info.setScore(0)
+mySprite = sprites.create(assets.image`12`, SpriteKind.Player)
+controller.moveSprite(mySprite, 150, 150)
+scene.setBackgroundColor(11)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.stairLarge)
+scene.cameraFollowSprite(mySprite)
+forever(function () {
+    if (info.score() < 0) {
+        game.over(false)
+    }
+})
