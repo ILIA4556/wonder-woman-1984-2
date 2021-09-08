741039e481bd1ec89ed23c399bcf0272bc97d912
ILIA4556 <87526128+ILIA4556@users.noreply.github.com>
2021-09-08 20:53:20 +0300

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -14,18 +14,28 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, l
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
     tiles.setTilemap(tilemap`4321`)
     tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleBlueCrystal)
+    tiles.setTileAt(location, assets.tile`transparency16`)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairWest, function (sprite, location) {
     game.over(true)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    info.setLife(game.askForNumber(""))
+    tiles.setTileAt(location, sprites.dungeon.chestOpen)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    info.stopCountdown()
     effects.clearParticles(mySprite)
 })
 info.onLifeZero(function () {
     game.over(false)
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    info.startCountdown(10)
     mySprite.startEffect(effects.fire)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.stairLarge, function (sprite, location) {
+    game.reset()
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
     tiles.setTilemap(tilemap`level3`)
@@ -39,7 +49,7 @@ info.setScore(0)
 mySprite = sprites.create(assets.image`12`, SpriteKind.Player)
 controller.moveSprite(mySprite, 150, 150)
 scene.setBackgroundColor(11)
-tiles.placeOnRandomTile(mySprite, sprites.dungeon.stairLarge)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.stairLadder)
 scene.cameraFollowSprite(mySprite)
 forever(function () {
     if (info.score() < 0) {
